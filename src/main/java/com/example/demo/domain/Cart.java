55ce0b1728051f9b8c811045b89a4f28ee12aad2
bahgaat <bahgat.reda90@gmail.com>
2022-09-15 09:36:12 +0200

    Add Cart Domain

---
+++ src/main/java/com/example/demo/domain/Cart.java
@@ -0,0 +1,33 @@
+package com.example.demo.domain;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int id;
+	
+	/*
+	 * @OneToMany(mappedBy = "cart") public List<CartProduct> cartProduct;
+	 */
+	
+	
+	
+	
+	
+}
